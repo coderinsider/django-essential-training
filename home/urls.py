4eb5bd9c39d43f83cdf3c0b4a9001975994f6461
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-11 14:58:22 +0630

    How can Django can list data with just some small changes

--- home/urls.py
+++ home/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('authorized', views.authorized, name='authorized'),
+    path('', views.HomeView.as_view()),
+    # path('', views.home, name='home'),
+    path('authorized', views.AuthorizedView.as_view()),
 ]
 
--- home/views.py
+++ home/views.py
@@ -2,12 +2,24 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
-def home(request):
-    dataArray = {'date': datetime.now().strftime("%b %d, %Y %H:%M:%S")}
-    return render(request, 'home/index.html', dataArray)
 
-@login_required(login_url='/admin/')
-def authorized(request):
-    dataArray = {}
-    return render(request, 'home/authorized.html', dataArray)
+class HomeView(TemplateView):
+    # pass the code
+    template_name = "home/index.html"
+    extra_content = {'currentDay': datetime.now().strftime("%b %d, %Y %H:%M:%S")}
+# def home(request):
+#     dataArray = {'date': datetime.now().strftime("%b %d, %Y %H:%M:%S")}
+#     return render(request, 'home/index.html', dataArray)
+
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    template_name = "home/authorized.html"
+    login_url = "/admin/"
+
+# @login_required(login_url='/admin/')
+# def authorized(request):
+#     dataArray = {}
+#     return render(request, 'home/authorized.html', dataArray)
