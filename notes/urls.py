15991ff530949ca3d61246a0337a09cfc80dfcb6
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-15 13:06:04 +0630

    The U in the CRUD - Updaating data

--- notes/urls.py
+++ notes/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailsView.as_view(), name="notes.detail"),
+    path('notes/<int:pk>/update', views.NotesUpdatedView.as_view(), name="notes.update"),
     path("notes/new", views.NotesCreateView.as_view(), name="notes.create")
 ]
--- notes/views.py
+++ notes/views.py
@@ -1,9 +1,15 @@
 from typing import List
 from django.shortcuts import render
 from django.http import Http404
-from django.views.generic import CreateView, DetailView, ListView
+from django.views.generic import CreateView, DetailView, ListView, UpdateView
 from .forms import NotesForm
 from .models import Note as Notes
+
+class NotesUpdatedView(UpdateView):
+    model = Notes
+    # fields = ['title', 'content']
+    success_url = "/mynote/notes"
+    form_class = NotesForm    
 class NotesCreateView(CreateView):
     model = Notes
     # fields = ['title', 'content']
