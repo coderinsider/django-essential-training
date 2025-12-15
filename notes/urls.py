30c08255e9cd74aa45d25a11e47a58a321eff86f
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-15 13:41:21 +0630

    The D in the CRUD - Delete data

--- notes/urls.py
+++ notes/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('notes', views.NotesListView.as_view(), name="notes.list"),
     path('notes/<int:pk>', views.NotesDetailsView.as_view(), name="notes.detail"),
     path('notes/<int:pk>/update', views.NotesUpdatedView.as_view(), name="notes.update"),
+    path('notes/<int:pk>/delete', views.NotesDeletedView.as_view(), name="notes.delete"),
     path("notes/new", views.NotesCreateView.as_view(), name="notes.create")
 ]
--- notes/views.py
+++ notes/views.py
@@ -2,8 +2,14 @@ from typing import List
 from django.shortcuts import render
 from django.http import Http404
 from django.views.generic import CreateView, DetailView, ListView, UpdateView
+from django.views.generic.edit import DeleteView
 from .forms import NotesForm
 from .models import Note as Notes
+class NotesDeletedView(DeleteView):
+    model = Notes
+    # fields = ['title', 'content']
+    success_url = "/mynote/notes"
+    template_name = "notes/note_delete.html"
 
 class NotesUpdatedView(UpdateView):
     model = Notes
