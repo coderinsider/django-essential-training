4e6f0243f9097716396be45829519998564c796f
Myat Ko Ko <coderinsider1@gmail.com>
2025-12-15 14:34:58 +0630

    How to update data stored in  your database

--- notes/models.py
+++ notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Note(models.Model):
@@ -7,3 +8,4 @@ class Note(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     likes  = models.PositiveBigIntegerField(default=0)
+    user  = models.ForeignKey(User, on_delete=models.CASCADE, related_name="notes")
